4b5806086c80518fecb605e2126dbfac939b41ef
Prince <princeagrawal024@gmail.com>
2020-09-20 20:09:47 +0530

    Added 1. Ureka server, 2. And added ureka client dependency to all the microservice (rating service, movie ingo and movie catalog service)

--- movie-info-service/src/main/java/com/demo/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/demo/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,10 @@ package com.demo.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //this is optional
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
